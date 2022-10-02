1b53157074ff9563dea1ec7fc1ff9db64bec5eb5
codescope <>
2022-10-02 08:38:53 +0200

    Part 03

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,16 +1,106 @@
-pub struct Scanner {}
+use std::string::String;
+
+pub struct Scanner {
+    source: String,
+    tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: usize,
+}
 
 impl Scanner {
-    pub fn new(_source: &str) -> Self {
-        Self {}
+    pub fn new(source: &str) -> Self {
+        Self {
+            source: source.to_string(),
+            tokens: vec![],
+            start: 0,
+            current: 0,
+            line: 1,
+        }
     }
 
-    pub fn scan_tokens(self: &Self) -> Result<Vec<Token>, String> {
+    pub fn scan_tokens(self: &mut Self) -> Result<Vec<Token>, String> {
+        let mut errors = vec![];
+        while !self.is_at_end() {
+            self.start = self.current;
+            match self.scan_token() {
+                Ok(_) => (),
+                Err(msg) => errors.push(msg),
+            }
+        }
+
+        self.tokens.push(Token {
+            token_type: Eof,
+            lexeme: "".to_string(),
+            literal: None,
+            line_number: self.line,
+        });
+
+        if errors.len() > 0 {
+            let mut joined = "".to_string();
+            errors.iter().map(|msg| {
+                joined.push_str(&msg);
+                joined.push_str("\n");
+            });
+            return Err(joined);
+        }
+
+        Ok(self.tokens.clone())
+    }
+
+    // var test = 0.01;
+
+    fn is_at_end(self: &Self) -> bool {
+        self.current >= self.source.len()
+    }
+
+    fn scan_token(self: &mut Self) -> Result<(), String> {
+        let c = self.advance();
+
+        match c {
+            '(' => self.add_token(LeftParen),
+            ')' => self.add_token(RightParen),
+            '{' => self.add_token(LeftBrace),
+            '}' => self.add_token(RightBrace),
+            ',' => self.add_token(Comma),
+            '.' => self.add_token(Dot),
+            '-' => self.add_token(Minus),
+            '+' => self.add_token(Plus),
+            ';' => self.add_token(Semicolon),
+            '*' => self.add_token(Star),
+            _ => return Err(format!("Unrecognized char at line {}: {}", self.line, c)),
+        }
+
         todo!()
+    }
+
+    fn advance(self: &mut Self) -> char {
+        let c = self.source.as_bytes()[self.current];
+        self.current += 1;
+
+        c as char
+    }
+
+    fn add_token(self: &mut Self, token_type: TokenType) {
+        self.add_token_lit(token_type, None);
+    }
+
+    fn add_token_lit(self: &mut Self, token_type: TokenType, literal: Option<LiteralValue>) {
+        let mut text = "".to_string();
+        let bytes = self.source.as_bytes();
+        for i in self.start..self.current {
+            text.push(bytes[i] as char);
+        }
+        self.tokens.push(Token {
+            token_type: token_type,
+            lexeme: text,
+            literal: literal,
+            line_number: self.line,
+        });
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum TokenType {
     // Single-char tokens
     LeftParen,
@@ -23,7 +113,7 @@ pub enum TokenType {
     Plus,
     Semicolon,
     Slash,
-    Start,
+    Star,
 
     // One Or Two Chars
     Bang,
@@ -60,8 +150,15 @@ pub enum TokenType {
 
     Eof,
 }
+use TokenType::*;
 
-#[derive(Debug)]
+impl std::fmt::Display for TokenType {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug, Clone)]
 pub enum LiteralValue {
     IntValue(i64),
     FValue(f64),
@@ -69,12 +166,12 @@ pub enum LiteralValue {
     IdentifierVal(String),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Token {
     token_type: TokenType,
     lexeme: String,
     literal: Option<LiteralValue>,
-    line_number: u64,
+    line_number: usize,
 }
 
 impl Token {
@@ -82,7 +179,7 @@ impl Token {
         token_type: TokenType,
         lexeme: String,
         literal: Option<LiteralValue>,
-        line_number: u64,
+        line_number: usize,
     ) -> Self {
         Self {
             token_type,
@@ -91,4 +188,15 @@ impl Token {
             line_number,
         }
     }
+
+    pub fn to_string(self: &Self) -> String {
+        format!("{} {} {:?}", self.token_type, self.lexeme, self.literal)
+    }
 }
+
+/*
+var test = 0.1;
+var test2 = test + 0.2;
+
+
+*/
