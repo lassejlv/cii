983b34078e8db0aceb098262b175d593134f7e1a
codescope <>
2022-11-04 21:13:15 +0100

    Part 13

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-mod scanner;
 mod expr;
-mod parser;
 mod interpreter;
-use crate::scanner::*;
-use crate::parser::*;
+mod parser;
+mod scanner;
+mod stmt;
 use crate::interpreter::*;
+use crate::parser::*;
+use crate::scanner::*;
+use crate::stmt::Stmt::*;
 
 use std::env;
 use std::fs;
 use std::io::{self, BufRead, Write};
 use std::process::exit;
 
-
 fn run_file(path: &str) -> Result<(), String> {
-    let mut interpreter = Interpreter::new();    
+    let mut interpreter = Interpreter::new();
     match fs::read_to_string(path) {
         Err(msg) => return Err(msg.to_string()),
         Ok(contents) => return run(&mut interpreter, &contents),
@@ -25,9 +26,8 @@ fn run(interpreter: &mut Interpreter, contents: &str) -> Result<(), String> {
     let tokens = scanner.scan_tokens()?;
 
     let mut parser = Parser::new(tokens);
-    let expr = parser.parse()?;
-    let result = interpreter.interpret(expr)?;
-    println!("{}", result.to_string());
+    let stmts = parser.parse()?;
+    interpreter.interpret(stmts);
     return Ok(());
 }
 
---
+++ src/stmt.rs
@@ -0,0 +1,7 @@
+use crate::expr::Expr;
+
+
+pub enum Stmt {
+    Expression { expression: Expr },
+    Print { expression: Expr},
+}
